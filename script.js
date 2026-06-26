7bcf357ca346294364bf606699d06fb9f0d74f1b
odavi8256-creator <odavi8256@gmail.com>
2026-06-26 03:26:47 -0300

    Criando tela de boas-vindas

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
 const botao = document.getElementById("entrar");
 
 botao.addEventListener("click", () => {
+
     document.getElementById("inicio").innerHTML = `
+
+    <div class="boasVindas">
+
         <h1>❤️ Bem-vinda, Madu ❤️</h1>
 
-        <p>
-            Esse não é um simples site...
+        <p class="frase">
+            Algumas pessoas entram na nossa vida e mudam tudo...
             <br><br>
-            É um pedacinho do meu coração feito especialmente para você.
+            Você foi uma delas.
         </p>
 
-        <button onclick="alert('Em breve você vai entrar na experiência completa ❤️')">
-            Continuar ❤️
+        <p class="frase2">
+            Esse presente foi feito com todo carinho do mundo.
+        </p>
+
+        <button id="comecar">
+            ❤️ Começar nossa história
         </button>
+
+    </div>
+
     `;
+
+    document.getElementById("comecar").addEventListener("click", () => {
+
+        document.getElementById("inicio").innerHTML = `
+
+        <h1>❤️ Nossa História ❤️</h1>
+
+        <p>
+            Em breve começa a nossa história...
+        </p>
+
+        `;
+
+    });
+
 });
