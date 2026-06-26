955597fcd849ce60c3f8ca88ca8832f11ea7708b
odavi8256-creator <odavi8256@gmail.com>
2026-06-26 05:47:42 -0300

    Corrigindo jogo 1

--- jogo1.js
+++ jogo1.js
@@ -1,35 +1,41 @@
 function iniciarJogo1() {
-
     document.getElementById("inicio").innerHTML = `
         <h1>🎮 Ache o ❤️</h1>
-
         <p>Clique no ❤️ para continuar!</p>
 
-        <div id="emojis" style="font-size:40px; line-height:70px;">
-            🌸 ⭐ 🍕 🦋 🎈 🌙<br>
-            🍀 ❤️ 🐱 🌻 🍩 🎁<br>
-            ⚽ 🍓 🌈 🧸 🐼 🎀
+        <div style="font-size:40px;">
+            <button>🌸</button>
+            <button>⭐</button>
+            <button>🍕</button>
+            <button>🦋</button>
+            <button>🎈</button>
+            <button>🌙</button>
+            <button>🍀</button>
+            <button id="coracao">❤️</button>
+            <button>🐱</button>
+            <button>🌻</button>
+            <button>🍩</button>
+            <button>🎁</button>
+            <button>⚽</button>
+            <button>🍓</button>
+            <button>🌈</button>
+            <button>🧸</button>
+            <button>🐼</button>
+            <button>🎀</button>
         </div>
     `;
 
-    const coracao = document.querySelector("#emojis");
+    document.getElementById("coracao").addEventListener("click", () => {
+        document.getElementById("inicio").innerHTML = `
+            <h1>🎉 Você encontrou meu coração! ❤️</h1>
 
-    coracao.addEventListener("click", (e) => {
-        if (e.target.textContent === "❤️") {
+            <button id="proximoJogo">
+                ➡️ Próxima etapa
+            </button>
+        `;
 
-            document.getElementById("inicio").innerHTML = `
-                <h1>🎉 Você encontrou meu coração! ❤️</h1>
-
-                <button id="proximoJogo">
-                    ➡️ Próxima etapa
-                </button>
-            `;
-
-            document.getElementById("proximoJogo").addEventListener("click", () => {
-                // Aqui vamos colocar o quiz depois.
-            });
-
-        }
+        document.getElementById("proximoJogo").addEventListener("click", () => {
+            // Aqui vamos colocar o quiz.
+        });
     });
-
 }
