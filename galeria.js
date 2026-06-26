c1ba65463147b7b7ab95eadce2f384429ad9868e
odavi8256-creator <odavi8256@gmail.com>
2026-06-26 05:01:10 -0300

    Criando galeria de fotos

--- galeria.js
+++ galeria.js
@@ -1,3 +1,33 @@
 function abrirGaleria() {
-    alert("✅ galeria.js está funcionando!");
+
+    document.getElementById("inicio").innerHTML = `
+        <h1>📸 Nossas Fotos ❤️</h1>
+
+        <img id="foto" src="imagens/fotomadu1.jpg" width="300">
+
+        <br><br>
+
+        <button id="proximaFoto">➡️ Próxima Foto</button>
+    `;
+
+    const fotos = [
+        "imagens/fotomadu1.jpg",
+        "imagens/fotomadu2.jpg",
+        "imagens/fotomadu3.jpg",
+        "imagens/fotoeu1.jpg",
+        "imagens/fotoeu2.jpg"
+    ];
+
+    let indice = 0;
+
+    document.getElementById("proximaFoto").addEventListener("click", () => {
+        indice++;
+
+        if (indice >= fotos.length) {
+            indice = 0;
+        }
+
+        document.getElementById("foto").src = fotos[indice];
+    });
+
 }
