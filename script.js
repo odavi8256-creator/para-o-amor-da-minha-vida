49c9e79af87b0296bcaf904a762426232a15a0c4
odavi8256-creator <odavi8256@gmail.com>
2026-06-26 03:32:33 -0300

    Atualizando script.js

--- script.js
+++ script.js
@@ -3,40 +3,57 @@ const botao = document.getElementById("entrar");
 botao.addEventListener("click", () => {
 
     document.getElementById("inicio").innerHTML = `
+        <div class="boasVindas">
+            <h1>❤️ Bem-vinda, Madu ❤️</h1>
 
-    <div class="boasVindas">
+            <p>
+                Algumas pessoas entram na nossa vida e mudam tudo...
+                <br><br>
+                Você foi uma delas.
+            </p>
 
-        <h1>❤️ Bem-vinda, Madu ❤️</h1>
-
-        <p class="frase">
-            Algumas pessoas entram na nossa vida e mudam tudo...
-            <br><br>
-            Você foi uma delas.
-        </p>
-
-        <p class="frase2">
-            Esse presente foi feito com todo carinho do mundo.
-        </p>
-
-        <button id="comecar">
-            ❤️ Começar nossa história
-        </button>
-
-    </div>
-
+            <button id="comecar">❤️ Começar nossa história</button>
+        </div>
     `;
 
     document.getElementById("comecar").addEventListener("click", () => {
 
         document.getElementById("inicio").innerHTML = `
+            <div class="historia">
+                <h1>❤️ Nossa História ❤️</h1>
 
-        <h1>❤️ Nossa História ❤️</h1>
-
-        <p>
-            Em breve começa a nossa história...
-        </p>
-
+                <p id="textoHistoria">
+                    Existem pessoas que passam pela nossa vida...
+                </p>
+            </div>
         `;
+
+        const texto = document.getElementById("textoHistoria");
+
+        setTimeout(() => {
+            texto.innerHTML = "E existem pessoas que mudam completamente a nossa vida...";
+        }, 2500);
+
+        setTimeout(() => {
+            texto.innerHTML = "Você fez isso comigo, Madu. ❤️";
+        }, 5000);
+
+        setTimeout(() => {
+            document.querySelector(".historia").innerHTML += `
+                <br><br>
+                <button id="proximo">❤️ Ver nossa história</button>
+            `;
+
+            document.getElementById("proximo").addEventListener("click", () => {
+                document.getElementById("inicio").innerHTML = `
+                    <h1>❤️ Em construção ❤️</h1>
+                    <p>
+                        Aqui vai entrar o contador, as fotos, a carta, a música e todas as surpresas.
+                    </p>
+                `;
+            });
+
+        }, 7500);
 
     });
 
