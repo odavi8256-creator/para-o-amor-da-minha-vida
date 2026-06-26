6c4ccae0ec3f23b279c91a3537afe7224bb093b5
odavi8256-creator <odavi8256@gmail.com>
2026-06-26 05:31:09 -0300

    Adicionando jogo 1

--- jogo1.js
+++ jogo1.js
@@ -1,1 +1,35 @@
+function iniciarJogo1() {
 
+    document.getElementById("inicio").innerHTML = `
+        <h1>🎮 Ache o ❤️</h1>
+
+        <p>Clique no ❤️ para continuar!</p>
+
+        <div id="emojis" style="font-size:40px; line-height:70px;">
+            🌸 ⭐ 🍕 🦋 🎈 🌙<br>
+            🍀 ❤️ 🐱 🌻 🍩 🎁<br>
+            ⚽ 🍓 🌈 🧸 🐼 🎀
+        </div>
+    `;
+
+    const coracao = document.querySelector("#emojis");
+
+    coracao.addEventListener("click", (e) => {
+        if (e.target.textContent === "❤️") {
+
+            document.getElementById("inicio").innerHTML = `
+                <h1>🎉 Você encontrou meu coração! ❤️</h1>
+
+                <button id="proximoJogo">
+                    ➡️ Próxima etapa
+                </button>
+            `;
+
+            document.getElementById("proximoJogo").addEventListener("click", () => {
+                // Aqui vamos colocar o quiz depois.
+            });
+
+        }
+    });
+
+}
